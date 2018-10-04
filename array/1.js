39fd920c86dd540e6d8c1f1a913b110ba3119a6a
siva <gnudivya@gmail.com>
2018-10-04 14:02:43 +0530

    array finish

---
+++ array/1.js
@@ -0,0 +1,21 @@
+// var dd = ["divi","priya","siva","sankar","uma","vishnu"];
+// for(var i=0;i<dd.length;i++){
+//     var ans = dd[i]
+//     console.log(ans)
+// }
+
+// var ff = [1,2,3,4,56,76];
+// for (let item of ff){
+//      var ans =item;
+//      console.log(item);
+// }
+var gg = [1,2,54,65,6785,865,86];
+// gg.forEach((dd)=>{
+//     var ans = dd;
+//     console.log(ans);
+// })
+
+map.gg( (a)=>{
+var ans = a
+})
+console.log(ans)
